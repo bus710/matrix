0b5744caf67028f75093773aa916884dc2334f11
bus710 <bus710@gmail.com>
2019-02-25 16:01:11 -0800

    added mdns for the server.

--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,7 @@ module matrix-server
 
 go 1.12
 
-require rsc.io/quote v1.5.2
+require (
+	github.com/hashicorp/mdns v1.0.0
+	rsc.io/quote v1.5.2
+)
--- server/main.go
+++ server/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "rsc.io/quote"
+	"fmt"
+	"os"
+
+	"github.com/hashicorp/mdns"
+	"rsc.io/quote"
 )
 
 func main() {
-    fmt.Println(quote.Hello())
+	fmt.Println(quote.Hello())
+
+	host, _ := os.Hostname()
+	info := []string{"My awesome service"}
+	service, _ := mdns.NewMDNSService(host, "_matrix_server._http", "", "", 8080, nil, info)
+
+	// Create the mDNS server, defer shutdown
+	server, _ := mdns.NewServer(&mdns.Config{Zone: service})
+	defer server.Shutdown()
+	for {
+
+	}
 }
