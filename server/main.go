47f4c7adf8a39afde4f56bd29687e74a399d6753
bus710 <bus710@gmail.com>
2019-03-06 09:50:00 -0800

    wip

--- server/main.go
+++ server/main.go
@@ -1,58 +1,28 @@
 package main
 
 import (
-	"context"
 	"log"
-	"os"
-	"os/signal"
 	"sync"
-	"syscall"
-	"time"
 
 	"rsc.io/quote"
 )
 
-type termSignal struct {
-	wait    *sync.WaitGroup
-	server  *webserver
-	sigterm chan os.Signal
-}
-
 func main() {
 	log.Println(quote.Hello())
 
-	var wait sync.WaitGroup
-	server := webserver{}
-	server.init()
+	// Declaring each struct
+	waitInstance := sync.WaitGroup{}
+	serverInstance := webserver{}
+	signalInstance := termSignal{}
 
-	sig := termSignal{
-		wait:   &wait,
-		server: &server,
-	}
+	// Initializing each module
+	serverInstance.init()
+	signalInstance.init(&waitInstance, &serverInstance)
 
-	wait.Add(1)
-	go sig.catcher()
+	// Increasing the wait group and starting each go routine
+	waitInstance.Add(1)
+	go signalInstance.catcher()
 
-	server.run()
-}
-
-func (sig *termSignal) catcher() {
-	sig.sigterm = make(chan os.Signal, 1)
-	signal.Notify(sig.sigterm, syscall.SIGINT, syscall.SIGTERM)
-
-	cleanup := func() {
-		ctx, cancel := context.WithTimeout(
-			context.Background(), time.Millisecond*3000)
-		defer cancel()
-		if err := sig.server.instance.Shutdown(ctx); err != nil {
-			log.Println(err)
-		}
-		sig.wait.Done()
-	}
-
-	select {
-	case received := <-sig.sigterm:
-		log.Println("\n>>", received)
-		cleanup()
-	}
+	// Running the webserver
+	serverInstance.run()
 }
---
+++ server/signal.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+type termSignal struct {
+	wait    *sync.WaitGroup
+	server  *webserver
+	sigterm chan os.Signal
+}
+
+// This function takes a WG, instance of service, and channels of go routines
+// and keeps the assigned params in its struct to access later.
+func (sig *termSignal) init(wait *sync.WaitGroup, serverInstance *webserver) {
+	sig.wait = wait
+	sig.server = serverInstance
+}
+
+// This is a handler to catch the interrupts from keyboard (CTRL+C)
+// and gracefully shuts down.
+func (sig *termSignal) catcher() {
+	// Storing the unbuffered channel in its struct.
+	sig.sigterm = make(chan os.Signal, 1)
+	// Connecting the keyboard signal to the channel.
+	signal.Notify(sig.sigterm, syscall.SIGINT, syscall.SIGTERM)
+
+	// Declaring a function that can be used running a graceful shutdown.
+	cleanup := func() {
+		ctx, cancel := context.WithTimeout(
+			context.Background(), time.Millisecond*3000)
+		defer cancel()
+		if err := sig.server.instance.Shutdown(ctx); err != nil {
+			log.Println(err)
+		}
+		sig.wait.Done()
+	}
+
+	// The routine waits here for the keyboard interrupt.
+	select {
+	case received := <-sig.sigterm:
+		log.Println("\n>>", received)
+		cleanup()
+	}
+}
