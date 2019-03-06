c31721b97a51082b3a2a1a1baefccdb6d5163923
bus710 <bus710@gmail.com>
2019-03-05 20:40:25 -0800

    started re-organizing the files.

---
+++ server/webserver.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/tidwall/gjson"
+	"golang.org/x/net/websocket"
+)
+
+// Item ...
+type Item struct {
+	ID      string `json:"id,omitempty"`
+	Content string `json:"content,omitempty"`
+}
+
+// Matrix ...
+type Matrix struct {
+	Meta string `json:"meta,omitempty"`
+	R64  string `json:"r64,omitempty"`
+	G64  string `json:"g64,omitempty"`
+	B64  string `json:"b64,omitempty"`
+}
+
+var item []Item
+
+// GetItem ...
+func GetItem(w http.ResponseWriter, r *http.Request) {
+	log.Println("GET is requested")
+	json.NewEncoder(w).Encode(item)
+}
+
+// PostItem ...
+func PostItem(w http.ResponseWriter, r *http.Request) {
+	log.Println("POST is requested")
+
+	var matrix Matrix
+
+	/* Workaroud when the incoming data's structure is not known */
+	// var m interface{}
+	// _ = json.NewDecoder(r.Body).Decode(&m)
+	// fmt.Println(m)
+
+	/* Although json.unmarshal can be used,
+	JSON.stringified string is much easier to handle. */
+	_ = json.NewDecoder(r.Body).Decode(&matrix)
+
+	matrixR64 := gjson.Get(matrix.R64, "r64").Array()
+	matrixG64 := gjson.Get(matrix.G64, "g64").Array()
+	matrixB64 := gjson.Get(matrix.B64, "b64").Array()
+
+	log.Println(matrix.Meta)
+	log.Println(matrixR64) // try matrixR64[0].Num
+	log.Println(matrixG64)
+	log.Println(matrixB64)
+
+	json.NewEncoder(w).Encode(item)
+}
+
+/* ========================================= */
+
+type message struct {
+	Message string `json:"message"`
+}
+
+func socket(ws *websocket.Conn) {
+	log.Println(ws.Request().RemoteAddr)
+
+	defer ws.Close()
+
+	for {
+		var m message
+		// receive a message using the codec
+		if err := websocket.JSON.Receive(ws, &m); err != nil {
+			log.Println(err)
+			break
+		}
+
+		log.Println("Received message:", m.Message)
+
+		// send a response
+		m2 := message{"Thanks for the message!"}
+		if err := websocket.JSON.Send(ws, m2); err != nil {
+			log.Println(err)
+			break
+		}
+	}
+}
+
+/* ========================================= */
